2bf5dab838fbfd2538ab5f57733533ae8e133adb
suraj.p_10 <91582438+surajp7875e@users.noreply.github.com>
2023-02-02 21:49:44 +0530

    Add files via upload

---
+++ longest common prefix.py
@@ -0,0 +1,54 @@
+Longest Common Prefix
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+
+
+
+class TrieNode:
+    def __init__(self):
+        self.children=[None]*26
+        self.isending=False
+        self.count=0
+        self.ind=0
+
+class Solution:
+    def __init__(self):
+        self.root=TrieNode()
+
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+
+
+        def insertt(val):
+            curr=self.root
+            for i in range(len(val)):
+                index=ord(val[i])-ord('a')
+                if not curr.children[index]:
+                    curr.children[index]=TrieNode()
+                    curr.count+=1
+                    curr.ind=index
+                curr=curr.children[index]
+            curr.isending=True
+
+        ans=''
+        def iteration(ans):
+            curr=self.root
+            flag=False
+            while not curr.isending:
+                if curr.count>1:
+                    return ans
+                elif curr.count==1:
+                    ans+=chr(curr.ind+ord('a'))
+                    curr=curr.children[curr.ind]
+            return ans
+        for i in strs:
+            insertt(i)
+        return iteration(ans)
